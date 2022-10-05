5370085fbbb373ca82d893c168963c898a6e4fd2
Edu Ruiz <ruizcantosps@gmail.com>
2022-10-05 22:50:04 +0200

    Initial commit

---
+++ script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+clear
+version=$(go version 2>/dev/null | awk '{print $3}' | tail -c +3)
+goVersion=$1
+unameOut=$(uname)
+
+function downloadVersion {
+  echo "[!] Download new version"
+  curl -O https://dl.google.com/go/go$goVersion.$unameOut-amd64.tar.gz
+  tar -xvf go$goVersion.$unameOut-amd64.tar.gz
+  sudo mv go /usr/local
+  rm -rf go$goVersion.$unameOut-amd64.tar.gz
+  clear
+  echo "[!] New version installed"
+  go version 2>/dev/null | awk '{print $3}'
+}
+
+function checkSO {
+  if [ $unameOut == 'Linux' ]; then
+    echo 'Linux'
+    downloadVersion
+  elif [ $unameOut == 'Darwin' ]; then
+    echo 'Mac'
+    downloadVersion
+  else
+    echo 'Error SO'
+  fi
+}
+
+function updateVersion {
+	pathGo=$(which go | rev | cut -c8- | rev)
+	sudo rm -rf $pathGo
+	echo "[!] go version removed"
+  checkSO
+}
+
+if [ $version == $goVersion 2>/dev/null ]; then
+  echo "[*] You have the version of go correctly"
+  go version 2>/dev/null | awk '{print $3}'
+else
+  echo "[!] Error version, updating now"
+	updateVersion
+fi
